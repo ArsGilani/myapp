cffa5fbd78f8c8304b8cb3297af5e22d10a70bfd
ArsGilani <gilani.vu@gmail.com>
2020-08-08 15:07:15 +0300

    добавил стили и картинки

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,3 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 
-//= require jquery3
-//= require popper
-//= require bootstrap
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,15 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-// require ("popper")
-// import "bootstrap-sprockets"
-// require('bootstrap-sprockets')
 
+
+import "bootstrap"
+import "../stylesheets/application"
+
+document.addEventListener("turbolinks:load", () => {
+	$('[data-toggle="tooltip"]').tooltip()
+	$('[data-toggle="popover"]').popover()
+})
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
